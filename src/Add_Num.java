c1b9544798ce18ac70f7be1c16ed2db6f87ecb39
TayyabaTariqstu <2022se1@student.uet.edu.pk>
2024-10-23 01:07:18 +0500

    Added Initial Sum Function

---
+++ src/Add_Num.java
@@ -0,0 +1,11 @@
+
+public class Add_Num {
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=20;
+        int result=a+b;
+        System.out.printf("Sum of two numbers is:"+result);
+
+    }
+}
